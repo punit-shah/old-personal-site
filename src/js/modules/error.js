497fa232720ea842b46a23e7a69ef1124ec681a4
Punit Shah <punit.shah@bbc.co.uk>
2016-11-11 00:40:05 +0000

    using 2 spaces for indentation

--- src/js/modules/error.js
+++ src/js/modules/error.js
@@ -4,12 +4,12 @@ const $error = $('.error img');
 const $bubble = $('.error__bubble');
 
 function dontTouchMe() {
-    $bubble
-        .fadeIn(250)
-        .delay(500)
-        .fadeOut(250);
+  $bubble
+    .fadeIn(250)
+    .delay(500)
+    .fadeOut(250);
 }
 
 export default function error() {
-    $error.on('click', dontTouchMe);
+  $error.on('click', dontTouchMe);
 }
