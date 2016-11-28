b51d5466cb03dece9a4932889c35a5b569db25be
Punit Shah <punit.shah@bbc.co.uk>
2016-11-28 00:54:55 +0000

    clicking the error selects random message from list

--- src/js/modules/error.js
+++ src/js/modules/error.js
@@ -3,13 +3,36 @@ import $ from 'jquery';
 const $error = $('.error img');
 const $bubble = $('.error__bubble');
 
-function dontTouchMe() {
+function getMessage() {
+  const errorMessages = [
+    '01001000010000010100100001000001',
+    'you need more ram',
+    'please do not touch me',
+    'lets make program',
+    'someone has stolen my trees',
+    'your trousers are on fire',
+    function time() {
+      return `the time is now ${Date.now()}`;
+    },
+  ];
+
+  const msg = errorMessages[Math.floor(Math.random() * errorMessages.length)];
+  if ($.isFunction(msg)) {
+    return msg();
+  } else {
+    return msg;
+  }
+}
+
+function showBubble() {
   $bubble
+    .hide()
+    .text(getMessage())
     .fadeIn(250)
     .delay(500)
     .fadeOut(250);
 }
 
 export default function error() {
-  $error.on('click', dontTouchMe);
+  $error.on('click', showBubble);
 }
